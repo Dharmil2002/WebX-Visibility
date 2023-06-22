416fe635bb011835e6cb6d36bc81effcccc772fb
Anjali <anjali24722@gmail.com>
2023-06-22 14:25:37 +0530

    Side menu added

--- src/app/layout/sidebar/sidebar-items.ts
+++ src/app/layout/sidebar/sidebar-items.ts
@@ -3,11 +3,64 @@ import { RouteInfo } from "./sidebar.metadata";
 export const ROUTES: RouteInfo[] = [
   {
     path: "Masters/Docket/Create",
-    title: "MENUITEMS.TASK.TEXT",
+    title: "Menu",
     moduleName: "CNote",
     icon: "check-circle",
     class: "",
     groupTitle: false,
     submenu: [],
   },
+  {
+    path: "dashboard/GlobeDashboardPage",
+    title: "Globe Dashboard Page",
+    moduleName: "Dashboard",
+    icon: "",
+    class: "",
+    groupTitle: false,
+    submenu:[]
+  },
+  {
+    path: "Masters/Docket/Ewaybill",
+    title: "Eway Bill",
+    moduleName: "Masters",
+    icon: "",
+    class: "",
+    groupTitle: false,
+    submenu:[]
+  },
+  {
+    path: "Masters/Docket/Create",
+    title: "Manual docket",
+    moduleName: "Masters",
+    icon: "",
+    class: "",
+    groupTitle: false,
+    submenu:[]
+  },
+  {
+    path: "Masters/Docket/Ewaybill-Config",
+    title: "Eway-Bill Config",
+    moduleName: "Masters",
+    icon: "",
+    class: "",
+    groupTitle: false,
+    submenu:[]
+  },
+  {
+    path: "Masters/Docket/LoadingSheet",
+    title: "Loading Sheet",
+    moduleName: "Masters",
+    icon: "",
+    class: "",
+    groupTitle: false,
+    submenu:[]
+  }, {
+    path: "Masters/Docket/ManifestGeneration",
+    title: "Manifest Generation ",
+    moduleName: "Masters",
+    icon: "",
+    class: "",
+    groupTitle: false,
+    submenu:[]
+  }
 ];

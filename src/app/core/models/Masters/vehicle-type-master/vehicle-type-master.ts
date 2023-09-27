6bf5c7a870409d93c96068badd3d1fbcbc680505
Ansu Singh <ansusingh.dcs22n@vnsgu.ac.in>
2023-09-27 18:07:05 +0530

    Vehicle type master changes

--- src/app/core/models/Masters/vehicle-type-master/vehicle-type-master.ts
+++ src/app/core/models/Masters/vehicle-type-master/vehicle-type-master.ts
@@ -1,64 +1,15 @@
 export class VehicleTypeMaster {
   vehicleTypeCode: string
-  pincodeCategory: number
-  area: string
-  serviceable: string
-  cityname: string
-  stateName: string;
-  activeFlag: string;
-  vehicleFillRate: number;
-  vehicleTypeName: string
-  vehicleManufacturerName: string
+  vehicleTypeName: any;
   vehicleTypeCategory: string
-  tankCapacity: number
   isActive: boolean
-  capacity: number
-  length: number
-  width: number
-  vehicleSize: number
-  height: number
-  capacityDiscount: number
-  tyreRotationAlertKMs: number
-  noOfPackages: number
-  averageSpeed: number
-  fixedCost: number
-  variableCost: number
-  availableFrom: number
-  availableTill: number
-  loadingTime: string
-  unloadingTime: string
-  weight: number
-  maxVisitingLocations: number
-  vehicleCategory: string
-  division: string
-  modelNo: any
-  tyreRotationatKm: any
-  typeDescription: any
-  grossVehicleWeight: any
-  unladenWeight: any
-  ratePerKM: any
   fuelType: any
   _id: string
+  oemmodel: any
   constructor(vehicleTypeMaster) {
     {
       this.vehicleTypeCode = vehicleTypeMaster.vehicleTypeCode || '';
-      this.pincodeCategory = vehicleTypeMaster.pincodeCategory || '';
-      this.area = vehicleTypeMaster.area || '';
-      this.serviceable = vehicleTypeMaster.serviceable || '';
-      this.stateName = vehicleTypeMaster.stateName || '';
-      this.cityname = vehicleTypeMaster.cityname || '';
-      this.activeFlag = vehicleTypeMaster.activeFlag || false;
-      this.vehicleFillRate = vehicleTypeMaster.vehicleFillRate || 0.0
-      this.averageSpeed = vehicleTypeMaster.averageSpeed || 0.0
-      this.fixedCost = vehicleTypeMaster.fixedCost || 0.0
-      this.variableCost = vehicleTypeMaster.variableCost || 0.0
-      this.availableFrom = vehicleTypeMaster.availableFrom || 0.0
-      this.availableTill = vehicleTypeMaster.availableTill || 0.0
-      this.loadingTime = vehicleTypeMaster.loadingTime || 0.0
-      this.unloadingTime = vehicleTypeMaster.unloadingTime || 0.0
-      this.weight = vehicleTypeMaster.weight || 0.0
-      this.maxVisitingLocations = vehicleTypeMaster.maxVisitingLocations || 0.0
-      this.vehicleSize = vehicleTypeMaster.vehicleSize || 0.0
+      this.isActive = vehicleTypeMaster.isActive || false;
     }
   }
 }

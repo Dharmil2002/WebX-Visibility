61b25bd736257da1447455bcab6cd15aeaaa6797
dhaval1325 <77376996+dhaval1325@users.noreply.github.com>
2023-10-12 17:53:20 +0530

    commit regarding to vehicle status

--- src/app/Masters/vehicle-status-update/vehicle-status-update-list/vehicle-status-update.component.ts
+++ src/app/Masters/vehicle-status-update/vehicle-status-update-list/vehicle-status-update.component.ts
@@ -10,7 +10,7 @@ import { formatDate } from 'src/app/Utility/date/date-utils';
 export class VehicleStatusUpdateComponent implements OnInit {
   tableLoad = true; // flag , indicates if data is still lodaing or not , used to show loading animation
   companyCode = parseInt(localStorage.getItem("companyCode"));
-  branchCode =localStorage.getItem("Branch");
+  branchCode = localStorage.getItem("Branch");
   addAndEditPath: string = 'Masters/Vehicle/Status/Add';
   data: [] | any;
   uploadComponent: any;
@@ -34,33 +34,46 @@ export class VehicleStatusUpdateComponent implements OnInit {
   columnHeader = {
     "srNo": "Sr No",
     "vehNo": "Vehicle No",
-    "driver":"Driver Name&Mobile",
-    "vendor":"Vendor Name",
-    "vendorType":"Vedor Type",
+    "driver": "Driver Name & Mobile",
+    "vendor": "Vendor Name  & Mobile",
+    "vendorType": "Vedor Type",
     "status": "Status",
     "tripId": "Trip Id",
     "currentLocation": "Location",
     "route": "Route",
-    "eta":"ETA",
-    "updateDt":"Last Updated Date time"
+    "eta": "ETA",
+    "updateDt": "Last Updated Date time"
+  };
+  columnWidths = {
+    'srNo': 'min-width: 1%',
+    'vehNo': 'min-width:9%',
+    'driver': 'min-width:9%',
+    'vendor': 'min-width:9%',
+    'vendorType': 'min-width:1%',
+    'status': 'align-self: center;min-width:6%;',
+    'tripId': 'min-width:16%',
+    'currentLocation': 'min-width:6%',
+    'route': 'min-width:9%',
+    'eta': 'min-width:13%',
+    'updateDt': 'min-width:13%'
   };
   headerForCsv = {
     "srNo": "Sr No",
     "vehNo": "Vehicle No",
-    "driveNM":"Drivename & Mobile",
-    "vendorname":"Vendor Name",
-    "vedortype":"Vedor Type",
+    "driveNM": "Drivename & Mobile",
+    "vendorname": "Vendor Name",
+    "vedortype": "Vedor Type",
     "status": "Status",
     "tripId": "Trip Id",
     "currentLocation": "Location",
     "route": "Route",
-    "eta":"ETA",
-    "updateDt":"Last Updated Date time"
+    "eta": "ETA",
+    "updateDt": "Last Updated Date time"
   };
   boxData: any;
   constructor(
     private _operationService: OperationService
-    ) {
+  ) {
     this.fetchVehicleStatus();
   }
 
@@ -73,11 +86,13 @@ export class VehicleStatusUpdateComponent implements OnInit {
       const vehicleStatusData = await getVehicleStatusFromApi(this.companyCode, this._operationService);
       // Assuming vehicleDetail.data is an array of objects with 'id' property
       // Generate srno for each object in the array
-      const vehicleTableData = vehicleStatusData.filter((x)=>x.currentLocation.trim()===this.branchCode.trim()).map((obj, index) => {
+      const vehicleTableData = vehicleStatusData.filter((x) => x.currentLocation.trim() === this.branchCode.trim()).map((obj, index) => {
         return {
           ...obj,
           srNo: index + 1,
-          route:`${obj.fromCity}-${obj.toCity}`
+          driver:`${obj?.driver || ""}-${obj?.dMobNo||""}`,
+          vendor:`${obj?.vendor || ""}-${obj?.vMobNo||""}`,
+          route: `${obj?.fromCity || ""}-${obj?.toCity || ""}`
         };
       });
       const sortedTableDetail = vehicleTableData.sort((a, b) => {
@@ -91,9 +106,9 @@ export class VehicleStatusUpdateComponent implements OnInit {
           return 0; // neither a nor b has updateDate, no preference for order
         }
       });
-      const tableDetail=sortedTableDetail.map((x)=>{if(x.updateDate){x.updateDt= formatDate(x.updateDate,'dd/MM/yyyy HH:mm'),x.eta= formatDate(x.updateDate,'dd/MM/yyyy HH:mm'),x.tripId=`${x.tripId} `}return x})
+      const tableDetail = sortedTableDetail.map((x) => { if (x.updateDate) { x.updateDt = formatDate(x.updateDate, 'dd/MM/yyyy HH:mm'), x.eta = formatDate(x.updateDate, 'dd/MM/yyyy HH:mm'), x.tripId = `${x.tripId} ` } return x })
 
-      this.tableData=tableDetail;
+      this.tableData = tableDetail;
       this.tableLoad = false;
       this.boxData = getVehicleDashboardDetails(this.tableData)
     } catch (error) {

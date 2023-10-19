e2880536f66bffdea180081d4fe355f6374e6331
dhaval1325 <77376996+dhaval1325@users.noreply.github.com>
2023-10-19 19:48:39 +0530

    commit regarding print issue

--- src/assets/FormControls/thc-generation.ts
+++ src/assets/FormControls/thc-generation.ts
@@ -116,7 +116,7 @@ export class thcControl {
                         }],
                     functions: {},
                     additionalData: {
-                        showNameAndValue: true,
+                        showNameAndValue: false,
                         metaData: "Basic"
                     }
                 },

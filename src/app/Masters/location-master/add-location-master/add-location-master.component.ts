d8f209d858b6afa0d6f6f1eb0374d9ff9d36233c
dhaval1325 <77376996+dhaval1325@users.noreply.github.com>
2024-01-10 09:06:17 +0530

    commit regarding to Location master

--- src/app/Masters/location-master/add-location-master/add-location-master.component.ts
+++ src/app/Masters/location-master/add-location-master/add-location-master.component.ts
@@ -451,7 +451,7 @@ export class AddLocationMasterComponent implements OnInit {
     
   }
   async getReportLocation() {
-    const data= await this.locationService.locationFromApi({locCode: {'D$regex' : `^${this.locationTableForm.controls.reportLoc.value}`,'D$options' : 'i'} ,locLevel: this.locationTableForm.controls.locLevel.value.value})
+    const data= await this.locationService.locationFromApi({locCode: {'D$regex' : `^${this.locationTableForm.controls.reportLoc.value}`,'D$options' : 'i'} ,locLevel: this.locationTableForm.controls.reportLoc.value.value})
     this.filter.Filter(
       this.jsonControlLocationArray,
       this.locationTableForm,

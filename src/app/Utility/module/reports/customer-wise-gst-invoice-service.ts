a725f6f07b2e00c219349da5ccf26becbbf60233
Ansu Singh <ansusingh.dcs22n@vnsgu.ac.in>
2024-02-01 16:29:54 +0530

    Added filters in customer gst report

--- src/app/Utility/module/reports/customer-wise-gst-invoice-service.ts
+++ src/app/Utility/module/reports/customer-wise-gst-invoice-service.ts
@@ -4,6 +4,7 @@ import { MasterService } from "src/app/core/service/Masters/master.service";
 import { StorageService } from "src/app/core/service/storage.service";
 import { formatDocketDate } from "../../commonFunction/arrayCommonFunction/uniqArray";
 import * as XLSX from 'xlsx';
+import { PayBasisdetailFromApi } from "src/app/Masters/Customer Contract/CustomerContractAPIUtitlity";
 @Injectable({
      providedIn: "root",
 })
@@ -13,7 +14,7 @@ export class CustGSTInvoiceService {
           private storage: StorageService
      ) { }
 
-     async getcustomerGstRegisterReportDetail(start, end) {
+     async getcustomerGstRegisterReportDetail(start, end, docNo) {
           const startValue = start;
           const endValue = end;
           const reqBody = {
@@ -21,20 +22,35 @@ export class CustGSTInvoiceService {
                collectionName: "cust_bill_headers",
                filter: {
                     cID: this.storage.companyCode,
-                    "D$and": [
-                         {
-                              "bGNDT": {
-                                   "D$gte": startValue
-                              }
-                         },
-                         {
-                              "bGNDT": {
-                                   "D$lte": endValue
-                              }
-                         }
-                    ]
                }
           }
+          // Check if the array contains only empty strings
+          const isEmptyDocNo = docNo.every(value => value === "");
+
+          // Add date range conditions if docNo are not present
+          if (isEmptyDocNo) {
+               reqBody.filter["D$and"] = [
+                    {
+                         "bGNDT": {
+                              "D$gte": startValue,
+                         },
+                    },
+
+                    {
+                         "bGNDT": {
+                              "D$lte": endValue,
+                         },
+                    }
+               ];
+          }
+
+          // Add docNo condition if docNoArray is present
+          if (!isEmptyDocNo) {
+               reqBody.filter["docNo"] = {
+                    "D$in": docNo,
+               };
+          }
+
           const res = await firstValueFrom(this.masterServices.masterMongoPost("generic/get", reqBody));
           reqBody.collectionName = "cust_bill_details"
           const rescustdetail = await firstValueFrom(this.masterServices.masterMongoPost("generic/get", reqBody));
@@ -43,64 +59,64 @@ export class CustGSTInvoiceService {
 
           let custstinvList = [];
 
-          res.data.map((element) => {
+          await Promise.all(res.data.map(async (element) => {
+
+               const isGSTINPresent = element?.cUST.gSTIN;
+               let paybasis = await PayBasisdetailFromApi(this.masterServices, 'PAYTYP');
+               paybasis = paybasis.find(x => x.value === element?.pAYBAS)
 
                const custdetailDet = rescustdetail.data ? rescustdetail.data.find((entry) => entry.bILLNO === element?.bILLNO) : null;
                const docketinvDet = resdocketinv.data ? resdocketinv.data.find((entry) => entry.dKTNO === custdetailDet?.dKTNO) : null;
-               let iGST = 0;
-               let cGST = 0;
+
                let invoiceNo = 0;
-               let invAmt = 0;
-               if (custdetailDet) {
-                    iGST = custdetailDet.iGST;
-                    cGST = custdetailDet.cGST;
-               }
+
                if (docketinvDet) {
                     invoiceNo = docketinvDet.iNVNO;
-                    invAmt = docketinvDet.iNVAMT
+                    //invAmt = docketinvDet.iNVAMT
                }
                let custgstinvData = {
                     "BILLNO": element?.bILLNO || '',
                     "BILLDT": formatDocketDate(element?.bGNDT || ''),
-                    "DocumentType": '',
+                    "DocumentType": 'General Bill',
                     "BILLSTATUS": element?.bSTSNM || '',
                     "BillGenState": element?.cUST.sT || '',
                     "BillBanch": element?.bLOC || '',
-                    "GSTRATE": element?.gST.rATE || '',
-                    "PayBasis": element?.pAYBAS || '',
-                    "Generation_GSTNO": element?.gST.iGST || '',
-                    "BillSubAt": element?.bLOC || '',
-                    "GSTTotal": element?.gST.aMT || '',
-                    "IGST": iGST,
-                    "CGST": cGST,
-                    "Currency": element?.CURR || '',
-                    "CurrencyAmount": element?.aMT || '',
+                    "Generation_GSTNO": element?.gEN.gSTIN || '',
                     "Party": `${element?.cUST.cD || ''} : ${element?.cUST.nM || ''}`,
-                    "PartyType": "",
+                    "PartyType": isGSTINPresent ? 'Registered' : 'UnRegistered',
                     "BillToState": element?.cUST.sT || '',
                     "PartyGSTN": element?.cUST.gSTIN || '',
+                    "BillSubAt": element?.bLOC || '',
                     "BusinessType": element?.bUSVRT || '',
-                    "Total_Taxable_Value": "",
+                    "Total_Taxable_Value": element?.gROSSAMT || 0.00,
                     "SAC": "",
+                    "GSTRATE": element?.gST.rATE || 0,
+                    "GSTTotal": element?.gST.aMT || 0.00,
                     "RCM": "",
-                    "SGSTUGST": "",
-                    "CNAMT": "",
-                    "TotalInvoice_Value": invAmt || 0.00,
-                    "TDSRate": "",
-                    "TDSAmount": "",
+                    "IGST": element?.gST.iGST || 0,
+                    "CGST": element?.gST.cGST || 0,
+                    "SGSTUGST": element?.gST.sGST || 0,
+                    "CNAMT": 0,
+                    "TotalInvoice_Value": element?.aMT || 0.00,
+                    "TDSRate": 0,
+                    "TDSAmount": 0.00,
                     "TDSSec": "",
                     "ReasonForIssue": "",
                     "InvoiceNo": invoiceNo || '',
                     "Manualno": invoiceNo || '',
-                    "ExchangeRate": "",
-                    "GstExempted": "",
+                    "Currency": element?.CURR || '',
+                    "ExchangeRate": 1,
+                    "CurrencyAmount": 0,
+                    "GstExempted": element.eXMT ? 'Yes' : 'No',
+                    "PayBasis": paybasis?.name || '',
                     "Narration": "",
-                    "UserId": "",
+                    "UserId": element?.eNTBY || '',
                     "ExemptionCategory": "",
                     "IrnNo": "",
                }
                custstinvList.push(custgstinvData)
-          })
+          }));
+
           return custstinvList;
      }
 }

f72f3d9a9f11760b960d7bb1211ba64d9f30162c
nishi mehta <nishimehta2711@gmail.com>
2024-03-22 16:32:28 +0530

    Issue resolve WT-178

--- src/app/Utility/module/reports/cash-bank-book-report-service.ts
+++ src/app/Utility/module/reports/cash-bank-book-report-service.ts
@@ -12,7 +12,6 @@ export class CashBankBookReportService {
           private storage: StorageService,) { }
 
      async getCashBankBook(detail) {
-
           let filter = {
                vNO: { D$in: detail.data.vNO },
           }
@@ -68,12 +67,13 @@ export class CashBankBookReportService {
                     },
                     {
                          D$addFields: {
+                              // transTypeADF: "$tTYPNM",
                               party: {
                                    D$concat: [
                                         {
                                              D$toString: "$pARTYCD",
                                         },
-                                        ":",
+                                        "-",
                                         "$pARTYNM",
                                    ]
                               },
@@ -192,34 +192,62 @@ export class CashBankBookReportService {
                               "legderAcc": { "D$ifNull": ["$ledger", ""] },
                               "debit": { "D$ifNull": ["$dR", ""] },
                               "credit": { "D$ifNull": ["$cR", ""] },
-                              "bal": { "D$ifNull": [0, ""] },
+                              // "bal": { "D$ifNull": [0, ""] },
+                              "bal": {
+                                   "D$sum": {
+                                        "D$subtract": ["$cR", "$dR"]
+                                   }
+                              },
                               "refNo": { "D$ifNull": ["$referenceNo", ""] },
                               // "refNo": { "D$ifNull": ["$refNo", ""] },
                               "rNO": { "D$ifNull": ["$rNO", "",], },
                               "dT": { "D$ifNull": ["$dT", "",], },
                               "doc": { "D$ifNull": ["$docNo", ""] },
                               "nar": { "D$ifNull": ["$nRT", ""] },
-                              "loc": { "D$ifNull": ["$lOC", ""] }
+                              "loc": { "D$ifNull": ["$lOC", ""] },
+                              "pMD": { "D$ifNull": ["$pMD", ""] }
                          }
-
                     }
                ]
           }
-
+          // Check if startAmt and endAmt exist
+          if (detail.data.startAmt && detail.data.endAmt) {
+               // Create a new filter object with D$or property
+               const orFilter = {
+                    D$or: [
+                         { cR: { D$gt: parseFloat(detail.data.startAmt), D$lt: parseFloat(detail.data.endAmt) } },
+                         { dR: { D$gt: parseFloat(detail.data.startAmt), D$lt: parseFloat(detail.data.endAmt) } }
+                    ]
+               };
+               // Merge the orFilter with the existing filter
+               reqBody.filters[0].D$match = { ...reqBody.filters[0].D$match, ...orFilter };
+          }
           const res = await firstValueFrom(this.masterService.masterMongoPost("generic/query", reqBody));
           if (res.data && res.data.length > 0) {
                let reportData: any[] = [];
-
-               reportData = res.data;
-               const total = res.data.reduce((accumulator, item) => {
-
-                    // Format date
-                    item.vDate = item.vDate ? moment(item.vDate).format('DD-MM-YYYY') : "";
-
-                    // Generate reference number if available
-                    if (item.rNO) {
-                         item.refNo = `Cheque ${item.rNO}, dated ${moment(item.dT).format("DD-MM-YYYY")}`;
+               // reportData = res.data;
+               // Conversion using map
+               reportData = res.data.map((item) => {
+                    return {
+                         ...item,
+                         debit: parseFloat(parseFloat(item.debit).toFixed(2)),
+                         credit: parseFloat(parseFloat(item.credit).toFixed(2)),
+                         bal: parseFloat(parseFloat(item.bal).toFixed(2)),
+                         vDate: item.vDate ? moment(item.vDate).format('DD-MM-YYYY') : "",
+                         refNo: item.rNO ? `${item.pMD} ${item.rNO}, dated ${moment(item.dT).format("DD-MM-YYYY")}` : ""
                     }
+               });
+
+               // Conversion back to floats using forEach
+               const total = reportData.reduce((accumulator, item) => {
+
+                    // // Format date
+                    // item.vDate = item.vDate ? moment(item.vDate).format('DD-MM-YYYY') : "";
+
+                    // // Generate reference number if available
+                    // if (item.rNO) {
+                    //      item.refNo = `Cheque ${item.rNO}, dated ${moment(item.dT).format("DD-MM-YYYY")}`;
+                    // }
 
                     // Update the totals
                     accumulator.totalDebit += item.debit ? parseFloat(item.debit) : 0.00;
@@ -236,9 +264,9 @@ export class CashBankBookReportService {
                     "vno": "",
                     "party": "",
                     "legderAcc": "Opening Balance :",
-                    "debit": "0.00",
-                    "credit": "0",
-                    "bal": "0",
+                    "debit": 0.00,
+                    "credit": 0.00,
+                    "bal": 0.00,
                     "refNo": "",
                     "doc": "",
                     "nar": "",
@@ -253,9 +281,9 @@ export class CashBankBookReportService {
                     "vno": "",
                     "party": "",
                     "legderAcc": "Total :",
-                    "debit": total.totalDebit.toFixed(2),
-                    "credit": total.totalCredit.toFixed(2),
-                    "bal": total.totalBalance.toFixed(2),
+                    "debit": parseFloat(total.totalDebit.toFixed(2)),
+                    "credit": parseFloat(total.totalCredit.toFixed(2)),
+                    "bal": parseFloat(total.totalBalance.toFixed(2)),
                     "refNo": "",
                     "doc": "",
                     "nar": "",
@@ -269,16 +297,16 @@ export class CashBankBookReportService {
 
 
      async getDetailCashBankBook(data) {
-          let filter = {}
-          if (data.endAmt) {
-               filter = { D$or: [{ cR: { D$gt: parseFloat(data.startAmt), D$lt: parseFloat(data.endAmt) } }, { dR: { D$gt: parseFloat(data.startAmt), D$lt: parseFloat(data.endAmt) } }] }
-          }
+          // let filter = {}
+          // if (data.endAmt) {
+          //      filter = { D$or: [{ cR: { D$gt: parseFloat(data.startAmt), D$lt: parseFloat(data.endAmt) } }, { dR: { D$gt: parseFloat(data.startAmt), D$lt: parseFloat(data.endAmt) } }] }
+          // }
           let matchQuery = {
                'D$and': [
                     { vDT: { 'D$gte': data.startValue } }, // Convert start date to ISO format
                     { vDT: { 'D$lte': data.endValue } },
                     { 'D$or': [{ cNL: false }, { cNL: { D$exists: false } }] },
-                    filter,
+                    // filter,
                     ...(data.Individual == "Y" ? [{ lOC: { 'D$in': [data.branch] } }] : []),
                     ...(data.accountCode.length > 0 ? [{ aCCCD: { 'D$in': data.accountCode } }] : [])
                ]
@@ -310,7 +338,7 @@ export class CashBankBookReportService {
                                         "default": ""
                                    }
                               },
-                              bal: { D$subtract: ["$dR", "$cR"] }
+                              bal: { D$subtract: ["$cR", "$dR"] }
                          }
                     } : {},
                     {
@@ -346,23 +374,23 @@ export class CashBankBookReportService {
                          return accumulator;
                     }, { totalMonDebit: 0, totalMonCredit: 0, totalMonBal: 0 });
                     reportData.forEach(element => {
-                         element.bal = element.bal?.toFixed(2) || "0.00";
-                         element.dr = element.dr?.toFixed(2) || "0.00";
-                         element.cr = element.cr?.toFixed(2) || "0.00";
+                         element.bal = element.bal?.toFixed(2) || 0.00;
+                         element.dr = element.dr?.toFixed(2) || 0.00;
+                         element.cr = element.cr?.toFixed(2) || 0.00;
                     });
                     if (data.reportType == "D") {
                          // Add opening balance row
                          const firstRow = {
                               "month": "Opening Balance :",
-                              "dr": "0.00",
-                              "cr": "0.00",
-                              "bal": "0.00"
+                              "dr": 0.00,
+                              "cr": 0.00,
+                              "bal": 0.00
                          };
                          reportData.unshift(firstRow);
                     }
                     else {
                          reportData.forEach(element => {
-                              element.openingBalance = "0.00"
+                              element.openingBalance = 0.00
                          });
                     }
                     const monTotalLastRow = {
@@ -370,7 +398,7 @@ export class CashBankBookReportService {
                          "dr": total.totalMonDebit.toFixed(2),
                          "cr": total.totalMonCredit.toFixed(2),
                          "bal": total.totalMonBal.toFixed(2),
-                         "openingBalance": "0.00"
+                         "openingBalance": 0.00
                     };
                     reportData.push(monTotalLastRow);
 

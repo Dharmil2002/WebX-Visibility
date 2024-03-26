bdb2ae5370ece1dec06a9d70b02ea9d0782efdc8
krupesh06 <130459793+krupesh06@users.noreply.github.com>
2024-03-26 13:08:34 +0530

    Main Screen Design

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -9,6 +9,8 @@ import { UpdateStockComponent } from "./ActionPages/update-stock/update-stock.co
 import { JobTrackerComponent } from "./tabs/job-tracker/job-tracker.component";
 import { AddDeliveryMrGenerationComponent } from "./tabs/Delivery MR Generation/add-delivery-mr-generation/add-delivery-mr-generation.component";
 import { DeliveryMrResponseModalComponent } from "./tabs/Delivery MR Generation/delivery-mr-response-modal/delivery-mr-response-modal.component";
+import { HomePageComponent } from "./HomePage/home-page/home-page.component";
+import { ReportDashboardComponent } from "./HomePage/report-dashboard/report-dashboard.component";
 const routes: Routes = [
   {
     path: "",
@@ -50,6 +52,15 @@ const routes: Routes = [
   {
     path:"DeliveryMrGeneration/Result",
     component:DeliveryMrResponseModalComponent
+  },
+
+  {
+    path:"home",
+    component:HomePageComponent
+  },
+  {
+    path:"ReportDashboard",
+    component:ReportDashboardComponent
   },
 
 ];

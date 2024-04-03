a9745c62b02606f169d4060d6734d8099582e35a
Ansu Singh <ansusingh.dcs22n@vnsgu.ac.in>
2024-04-03 12:40:08 +0530

    Vendor Master Bulk upload completed

--- src/app/core/service/Utility/xlsx Utils/xlsxutility.service.ts
+++ src/app/core/service/Utility/xlsx Utils/xlsxutility.service.ts
@@ -21,7 +21,7 @@ export class xlsxutilityService {
       for (const rule of rules) {
         const value = item[rule.ItemsName];
         for (const validation of rule.Validations) {
-          if ("Required" in validation && validation.Required && !value.trim()) {
+          if ("Required" in validation && validation.Required && !value) {
             errors.push(`${rule.ItemsName} is required.`);
           }
           if ("dateLimit" in validation && value) {

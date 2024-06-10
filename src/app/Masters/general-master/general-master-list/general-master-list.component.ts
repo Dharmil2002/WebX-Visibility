d3c400063e3b94611caf3ac731e31be4d155a1b0
mithawalahasan <hasanmithawala3@gmail.com>
2024-06-10 11:58:29 -0700

    Commit regarding WT-869 (Hasan) Master-Misc-Changes

--- src/app/Masters/general-master/general-master-list/general-master-list.component.ts
+++ src/app/Masters/general-master/general-master-list/general-master-list.component.ts
@@ -1,30 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { MasterService } from 'src/app/core/service/Masters/master.service';
-import { StorageService } from 'src/app/core/service/storage.service';
+import { Component, OnInit } from "@angular/core";
+import { MasterService } from "src/app/core/service/Masters/master.service";
+import { StorageService } from "src/app/core/service/storage.service";
 
 @Component({
-  selector: 'app-general-master-list',
-  templateUrl: './general-master-list.component.html',
+  selector: "app-general-master-list",
+  templateUrl: "./general-master-list.component.html",
 })
 export class GeneralMasterListComponent implements OnInit {
   csv: any[];
   tableLoad = true; // flag , indicates if data is still lodaing or not , used to show loading animation
-  toggleArray = []
+  toggleArray = [];
   companyCode: any = 0;
   linkArray = [];
+  csvFileName:any;
   columnHeader = {
-    "srNo": "Sr No",
-    "headerDesc": "General Master",
-    "description": "Description",
-    "whereused": "Masters/Forms where used",
-    "actions": "Actions"
+    srNo: {
+      Title: "Sr No",
+      class: "matcolumncenter",
+      Style: "max-width:150px",
+    },
+    headerDesc: {
+      Title: "General Master",
+      class: "matcolumnleft",
+      Style: "max-width:150px",
+    },
+    description: {
+      Title: "Description",
+      class: "matcolumnleft",
+      Style: "max-width:150px",
+    },
+    whereused: {
+      Title: "Masters/Forms where used",
+      class: "matcolumnleft",
+      Style: "max-width:150px",
+    },
+    actions: {
+      Title: "Actions",
+      class: "matcolumnleft",
+      Style: "max-width:100px",
+    },
+    // "srNo": "Sr No",
+    // "headerDesc": "General Master",
+    // "description": "Description",
+    // "whereused": "Masters/Forms where used",
+    // "actions": "Actions"
   };
   headerForCsv = {
-    "srNo": "Sr No",
-    "general": "General Master",
-    "description": "Description",
-    "formsUsed": "Masters/Forms where used",
-  }
+    srNo: "Sr No",
+    headerDesc: "General Master",
+    description: "Description",
+    whereused : "Masters/Forms where used",
+  };
+  staticField = ["srNo", "headerDesc", "description", "whereused"];
   breadScrums = [
     {
       title: "General Master",
@@ -35,38 +62,50 @@ export class GeneralMasterListComponent implements OnInit {
   dynamicControls = {
     add: false,
     edit: true,
-    csv: false
-  }
+    csv: false,
+  };
   addAndEditPath: string;
   tableData: any[];
-  constructor(private masterService: MasterService, private storage: StorageService) {
+  constructor(
+    private masterService: MasterService,
+    private storage: StorageService
+  ) {
     this.companyCode = this.storage.companyCode;
-   }
+  }
   ngOnInit(): void {
+    this.csvFileName="General Master"
     this.addAndEditPath = "/Masters/GeneralMaster/GeneralMasterCodeList";
     this.getGeneralDetails();
   }
   getGeneralDetails() {
     let req = {
       companyCode: this.storage.companyCode,
-      "collectionName": "CodeTypes",
-      "filter": {}
-    }
-    this.masterService.masterPost('generic/get', req).subscribe({
+      collectionName: "CodeTypes",
+      filter: {companyCode: this.storage.companyCode},
+    };
+    this.masterService.masterPost("generic/get", req).subscribe({
       next: (res: any) => {
         if (res) {
           // Generate srno for each object in the array
           const dataWithSrno = res.data.map((obj, index) => {
             return {
               ...obj,
-              srNo: index + 1
+              srNo: index + 1,
             };
           });
-          this.csv = dataWithSrno
+          this.csv = dataWithSrno;
           this.tableLoad = false;
         }
-      }
-    })
+      },
+    });
   }
+  functionCallHandler($event) {
+    let functionName = $event.functionName;
+    try {
+      this[functionName]($event);
+    } catch (error) {
+      console.log("failed");
+    }
+  }
+ 
 }
-
--- src/app/Utility/commonFunction/common.ts
+++ src/app/Utility/commonFunction/common.ts
@@ -10,21 +10,32 @@ export function isValidNumber(value: any): boolean {
            || typeof value === 'string' && !isNaN(Number(value));
 }  
 
+// export function isValidDate(value: any): boolean {
+//   if (value instanceof Date) { 
+//     return true;
+//   } else {
+//     const date = new Date(value);
+//     const tm = date.getTime();
+//     return !isNaN(tm) && tm > 0;
+//     // if (isNaN(tm) || tm <= 0) {
+//     //     return false; // Not a date
+//     // }
+
+//     // const dt = parseISO(value);
+//     // return isValid(dt);
+//   }
+// }
 export function isValidDate(value: any): boolean {
-  if (value instanceof Date) { 
+  if (value instanceof Date) {
     return true;
   } else {
-    const date = new Date(value);
-    const tm = date.getTime();
-    return !isNaN(tm) && tm > 0;
-    // if (isNaN(tm) || tm <= 0) {
-    //     return false; // Not a date
-    // }
-
-    // const dt = parseISO(value);
-    // return isValid(dt);
+    const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
+    dateFormatRegex.test(value) && !isNaN(new Date(value).getTime());
   }
+  const dt = parseISO(value);
+  return isValid(dt);
 }
+
 
 export function ConvertToDate(value: any): Date {
     return isValidDate(value) ? new Date(value) : undefined;

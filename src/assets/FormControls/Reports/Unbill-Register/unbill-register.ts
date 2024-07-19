24a46b76af5f2aa179001ef6f131a37e4e552652
Manan0504 <manan.sanghani@webxpress.in>
2024-07-19 17:38:15 +0530

    redevelop unbill register report

--- src/assets/FormControls/Reports/Unbill-Register/unbill-register.ts
+++ src/assets/FormControls/Reports/Unbill-Register/unbill-register.ts
@@ -21,29 +21,111 @@ export class billRegControl {
                     },
                },
                {
-                    name: 'loc',
-                    label: 'From Location',
-                    placeholder: 'From Location',
-                    type: 'multiselect', value: '', filterOptions: "", autocomplete: "", displaywith: "",
+                    name: "loc",
+                    label: "Location",
+                    placeholder: "Select Location",
+                    type: "dropdown",
+                    value: [],
+                    filterOptions: "",
+                    autocomplete: "",
+                    displaywith: "",
                     Validations: [
+                         {
+                              name: "autocomplete",
+                         },
+                         {
+                              name: "invalidAutocompleteObject",
+                              message: "Choose proper value",
+                         },
                     ],
                     additionalData: {
-                         support: "locHandler",
-                         showNameAndValue: false,
+                         showNameAndValue: true,
                     },
-                    functions: {
-                         onToggleAll: "toggleSelectAll",
-                    },
-                    generatecontrol: true, disable: false
+                    generatecontrol: true,
+                    disable: false,
                },
                {
-                    name: 'locHandler',
-                    label: 'locHandler',
-                    placeholder: 'locHandler',
-                    type: '',
-                    value: '',
+                    name: "Individual",
+                    label: "",
+                    placeholder: "Individual",
+                    type: "radiobutton",
+                    value: [
+                         { value: "Y", name: "Individual" },
+                         { value: "N", name: "Cumulative" },
+                    ],
+                    generatecontrol: true,
+                    disable: false,
                     Validations: [],
-                    generatecontrol: false, disable: false
+               },
+               {
+                    name: "CUST",
+                    label: "Customer",
+                    placeholder: "Select Customer",
+                    type: "dropdown",
+                    value: "",
+                    generatecontrol: true,
+                    disable: false,
+                    Validations: [
+                         {
+                              name: "autocomplete",
+                         },
+                         {
+                              name: "invalidAutocompleteObject",
+                              message: "Choose proper value",
+                         },
+                    ],
+                    additionalData: {
+                         showNameAndValue: true,
+                    },
+                    functions: {
+                         onModel: "getCustomer",
+                    },
+               },
+               {
+                    name: "payType",
+                    label: "Payment Type",
+                    placeholder: "Payment Type",
+                    type: "dropdown",
+                    value: [],
+                    filterOptions: "",
+                    autocomplete: "",
+                    displaywith: "",
+                    generatecontrol: true,
+                    disable: false,
+                    functions: {},
+                    Validations: [
+                         {
+                              name: "autocomplete",
+                         },
+                         {
+                              name: "invalidAutocompleteObject",
+                              message: "Choose proper value",
+                         },
+                    ],
+                    additionalData: {
+                         showNameAndValue: true,
+                    },
+               },
+               {
+                    name: 'loadType',
+                    label: 'Load Type',
+                    placeholder: '',
+                    type: 'Staticdropdown',
+                    value: [
+                         { value: "LTL", name: "LTL" },
+                         { value: "FTL", name: "FTL" },
+                         { value: "All", name: "Both" },
+                    ],
+                    filterOptions: "",
+                    autocomplete: "",
+                    displaywith: "",
+                    Validations: [],
+                    additionalData: {
+                         showNameAndValue: false,
+                    },
+                    functions: {},
+                    generatecontrol: true,
+                    disable: false
                },
                {
                     name: "end",
@@ -56,7 +138,7 @@ export class billRegControl {
                     generatecontrol: false,
                     disable: true,
                     Validations: [],
-               }
+               },
           ]
      }
 }
